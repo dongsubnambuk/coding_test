032b6989115467a7bb33982e64042608090bbf1c
스바스바동스바 <105368619+dongsubnambuk@users.noreply.github.com>
2025-07-24 02:45:00 +0900

    [level 1] Title: 소수 찾기, Time: 202.25 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,24 +1,20 @@
 class Solution {
     public int solution(int n) {
-       boolean prime;
-        int answer = 0;
-        for(int i=2;i<=n;i++){ 
- 
-            prime=true;
-            for(int j=2;j<=Math.sqrt(i);j++){
-                
-                if(i%j==0){
-                    prime=false;
-                    break;
-                }
+       int count = 1;
+        
+         for (int i = 3; i <= n; i++) {
+            if (isPrime(i)){
+                count++;
             }
-            
-            if(prime==true){
-                answer++;
-            }
-        }
- 
-        return answer;
+         }
+        return count;
     }
     
+      public boolean isPrime(int n) {
+
+        for (int i = 2; i < n; i++) {		// 2보다 크고 n보다 작은 수로 나눈다.
+            if (n % i == 0) return false;		// 만약 나누어 떨어지는 것이 있다면 false 반환
+        }
+        return true;				// 모두 나누고도 떨어지는 수가 없다면 true 반환
+    }
 }
