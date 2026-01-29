ada070de4f06a8951884ad7eebe2757a90321040
스바스바동스바 <105368619+dongsubnambuk@users.noreply.github.com>
2026-01-29 21:35:10 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 164 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = 0;
+
+        for (int i = 1; i <= Math.min(a,b); i++) {
+            if (a % i == 0 && b % i == 0)
+                result =i;
+        }
+        int min = (a*b)/result;
+
+        System.out.println(result);
+        System.out.println(min);
+
+    }
+}
