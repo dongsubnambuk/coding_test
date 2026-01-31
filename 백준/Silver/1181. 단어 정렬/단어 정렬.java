bc2c3076a44868435adff6b4e872cb9f6199df8f
스바스바동스바 <105368619+dongsubnambuk@users.noreply.github.com>
2026-01-31 17:08:25 +0900

    [Silver V] Title: 단어 정렬, Time: 664 ms, Memory: 37960 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] words = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            words[i] = sc.next();
+        }
+
+        Arrays.sort(words, (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return o1.length() - o2.length();
+        });
+
+        System.out.println(words[0]);
+        for (int i = 1; i < n; i++) {
+            if (!words[i].equals(words[i - 1])) {
+                System.out.println(words[i]);
+            }
+        }
+    }
+}
